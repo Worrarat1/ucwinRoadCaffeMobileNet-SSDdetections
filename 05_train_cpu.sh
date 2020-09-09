d7869d4507fe1b88d77cbb63f93ba634ced2cc08
Worrarat Prasertsil <panworrarat@gmail.com>
2020-09-09 11:57:46 +0700

    added train with CPU .sh

---
+++ 05_train_cpu.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+
+caffessd_root_dir=$(./readconfig_ini_file.py settings-config.ini DEFAULT caffessd_root_dir)
+
+$cd $caffessd_root_dir
+
+if ! test -f example/MobileNetSSD_train.prototxt ;then
+	echo "error: example/MobileNetSSD_train.prototxt does not exist."
+	echo "please use the gen_model.sh to generate your own model."
+        exit 1
+fi
+mkdir -p snapshot
+$caffessd_root_dir/build/tools/caffe train -solver="solver_train.prototxt" \
+-weights="mobilenet_iter_73000.caffemodel"
